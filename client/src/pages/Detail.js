2cfe4226be9ae66025887e0032d8f6913fa57de4
Anna Kaser <kaserann96@gmail.com>
2021-01-13 19:06:27 -0700

    shopping cart functionality

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,7 +5,12 @@ import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif';
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from "../utils/actions";
 import Cart from '../components/Cart';
 
 function Detail() {
@@ -16,7 +21,24 @@ function Detail() {
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const { products, cart } = state;
+
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 }
+      });
+    }
+  };
 
   useEffect(() => {
     if (products.length) {
@@ -28,6 +50,13 @@ function Detail() {
       });
     }
   }, [products, data, dispatch, id]);
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
+    });
+  };
 
   return (
     <>
@@ -47,10 +76,11 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
-              Add to Cart
-            </button>
-            <button>
+            <button onClick={addToCart}>Add to cart</button>
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
               Remove from Cart
             </button>
           </p>
